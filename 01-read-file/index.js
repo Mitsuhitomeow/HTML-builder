eb73f4a02b6b6e865759be983f916ed287e0dee7
Alexey Melnikov <cancem343@gmail.com>
2024-01-17 17:36:19 +0300

    feat: add a solution to the 01-read-file by HTML-builder

---
+++ 01-read-file/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+
+const readStream = fs.createReadStream(
+  path.join(__dirname, 'text.txt'),
+  'utf8',
+);
+
+readStream.on('data', (chunk) => {
+  console.log(chunk);
+});
+
+readStream.on('end', () => {
+  readStream.close();
+});
+
+readStream.on('error', (err) => {
+  console.error('error', err);
+});
