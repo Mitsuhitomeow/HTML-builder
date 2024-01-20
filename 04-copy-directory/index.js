c53b94a490be371c1cc6258e8ae294a79564c556
Alexey Melnikov <cancem343@gmail.com>
2024-01-20 12:00:22 +0300

    fix: fix the solution, when deleting the original, after running the code, the copy is now deleted as well

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,11 +4,26 @@ const fs = require('fs').promises;
 const originDirPath = path.resolve(__dirname, 'files');
 const copyDirPath = path.resolve(__dirname, 'files-copy');
 
+const deleteFiles = async (filePath) => {
+  const copyDir = await fs.readdir(filePath);
+
+  try {
+    await Promise.all(
+      copyDir.map(async (file) => {
+        await fs.unlink(path.join(filePath, file));
+      }),
+    );
+  } catch (err) {
+    console.log('Error delete files:', err);
+  }
+};
+
 const copyDir = async () => {
   try {
     const files = await fs.readdir(originDirPath);
-    await fs.mkdir(copyDirPath, { recursive: true });
 
+    await fs.mkdir(copyDirPath, { recursive: true });
+    await deleteFiles(copyDirPath);
     await Promise.all(
       files.map(async (file) => {
         const originFilePath = path.join(originDirPath, file);
